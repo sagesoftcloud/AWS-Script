abe074f804e1e3f3da6a4150cb8c65915b632acb
John Chris Pensica Constantino <79827782+jcconstantino@users.noreply.github.com>
2023-07-31 13:54:04 +0800

    Update aws-replication.sh

--- aws-replication.sh
+++ aws-replication.sh
@@ -1,2 +1,3 @@
+#bin/bash
 wget -O ./aws-replication-installer-init https://aws-application-migration-service-us-east-1.s3.us-east-1.amazonaws.com/latest/linux/aws-replication-installer-init
 sudo python3 aws-replication-installer-init
