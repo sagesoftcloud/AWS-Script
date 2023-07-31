2d0cf19bff1b784e0a65dae7873a486f5478083b
John Chris Pensica Constantino <79827782+jcconstantino@users.noreply.github.com>
2023-07-31 15:09:13 +0800

    Update discovery-agent.sh

--- discovery-agent.sh
+++ discovery-agent.sh
@@ -1,4 +1,10 @@
 #bin/bash
+
+
+#install the agent
 curl -o ./aws-discovery-agent.tar.gz https://s3-us-east-1.amazonaws.com/aws-discovery-agent.us-east-1/linux/latest/aws-discovery-agent.tar.gz
 tar -xzf aws-discovery-agent.tar.gz
 sudo bash install -r us-east-1 -k <ACCESS KEY> -s <Secret Access Key>
+
+#uninstall the agent
+apt-get remove aws-discovery-agent:i386 -y
