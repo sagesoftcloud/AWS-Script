f34f180e66c6664612796ba8f10a9bb4b9f3005c
sagesoftcloud <142279554+sagesoftcloud@users.noreply.github.com>
2023-09-20 09:57:12 +0800

    Create run-the-docker.sh

---
+++ run-the-docker.sh
@@ -0,0 +1,11 @@
+Dockers restart
+1. Login to the server via SSH
+2. run "docker ps"
+3. There should be 3 running images.
+4. if one or more is missiing
+5. for frontend go to: /var/dockers/csd/frontend
+6. for backend go to: /var/dockers/csd/backend
+7.for postgres : /var/docker/postgres
+8. then run "bash runner.sh
+
+9. check if docker and nginx is running.
